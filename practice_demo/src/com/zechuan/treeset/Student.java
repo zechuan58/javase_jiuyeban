1d7b288d6f96467240948b5a5b90b6fca4da6f4f
zehchuan <1195948307@qq.com>
2022-08-20 00:15:41 +0800

    TreeSet中的compareTo方法的重写与运用

---
+++ practice_demo/src/com/zechuan/treeset/Student.java
@@ -0,0 +1,52 @@
+package com.zechuan.treeset;
+
+public class Student implements Comparable<Student> {
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    /*
+    重写接口compare中的compareTo方法
+     */
+    @Override
+    public int compareTo(Student o) {
+        //按照对象的年龄进行排序
+        //age是主要判断条件
+        int result = this.age - o.age;
+        //如果年龄一样,就按照对象的姓名排序,调用String类中的compareTo方法比较年龄
+        //name是次要比较条件
+        result = result == 0 ? this.name.compareTo(o.name) : result;
+        return result;
+    }
+}
---
+++ practice_demo/src/com/zechuan/treeset/TreeSetCompareTo.java
@@ -0,0 +1,20 @@
+package com.zechuan.treeset;
+
+import java.util.TreeSet;
+
+/**
+ * TreeSet集合存储自定义Student类型
+ */
+public class TreeSetCompareTo {
+    public static void main(String[] args) {
+        TreeSet<Student> ts=new TreeSet<>();
+        Student s1=new Student("小明",21);
+        Student s2=new Student("小明明",19);
+        Student s3=new Student("小小明",23);
+
+        ts.add(s1);
+        ts.add(s2);
+        ts.add(s3);
+        System.out.println(ts);
+    }
+}
