f1475b328c9df9cc354c6e18dfee9950351cd428
zehchuan <1195948307@qq.com>
2022-08-20 17:05:07 +0800

    可变参数

--- day06_map/src/com/itheima/d2_variable_param/Demo1.java
+++ day06_map/src/com/itheima/d2_variable_param/Demo1.java
@@ -10,19 +10,19 @@ package com.itheima.d2_variable_param;
  */
 public class Demo1 {
     public static void main(String[] args) {
-        int sum1 = sum(3, 4, 5);
+        int sum1 = sum(1, 2, 3, 4);
         System.out.println(sum1);
 
-        int sum2 = sum(3, 4, 5, 6, 6, 7, 8, 9, 10);
+        int sum2 = sum(10, 20, 30, 40, 50);
         System.out.println(sum2);
     }
 
     //int... arr： 表示可变参数，可以接收多个int类型的值
-    public static int sum(int... arr){
+    public static int sum(int... arr) {
         //这里的arr本质上是一个数组，多个参数就表示数组中的元素
         int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum+=arr[i];
+        for (int i : arr) {
+            sum += i;
         }
         return sum;
     }
