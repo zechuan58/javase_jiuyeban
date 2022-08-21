2742cca89bc5c1017d368313c8f22179b8396c18
zehchuan <1195948307@qq.com>
2022-08-21 11:17:30 +0800

    LinkedHashSet集合的使用场景

--- practice_demo/src/com/practice/day6_demo/PhoneTest.java
+++ practice_demo/src/com/practice/day6_demo/PhoneTest.java
@@ -1,13 +1,15 @@
 package com.practice.day6_demo;
 
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 /**
+ * 定义一个Phone类，有品牌brand，价格price两个属性
+ * 选择合适的集合，往该集合一中添加3个Phone对象，要求元素不能重复，并且保证存取顺序
  * Phone测试类
  */
 public class PhoneTest {
     public static void main(String[] args) {
-        HashSet<Phone> set = new HashSet<>();
+        LinkedHashSet<Phone> set = new LinkedHashSet<>();
         set.add(new Phone("Iphone", 5499));
         set.add(new Phone("VIVO", 3499));
         set.add(new Phone("OPPO", 2499));
